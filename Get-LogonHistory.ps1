a6d310822fe545e51aa2454003c852f75d1c066e
Craig Meinschein <cmeinsch@gmail.com>
2013-03-04 18:02:15 -0800

    Parses logons successfully.

--- Get-LogonHistory.ps1
+++ Get-LogonHistory.ps1
@@ -4,4 +4,15 @@
 $events = Get-EventLog Security -AsBaseObject -InstanceId 4624,4647
 $logons = $events | Where-Object { ($_.InstanceId -eq 4647) -or (($_.InstanceId -eq 4624) -and ($_.Message -match "Logon Type:\s+2")) }
 
-$logons
+foreach($logon in $logons) {
+	if ($logon.InstanceId -eq 4624) {
+		$logonType = 'on'
+		
+		$logon.message -match "New Logon:\s*Security ID:\s*.*\s*Account Name:\s*(\w+)"
+		$user = $matches[1]
+		$time = Get-Date $logon.TimeGenerated
+		Write-Host "$user,$logonType,$time"
+	} else {
+		# This is a logoff.
+	}
+}
