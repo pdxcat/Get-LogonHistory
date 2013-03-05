d16dd44f1334d18a2323cb8239b1585fc47c45aa
Craig Meinschein <cmeinsch@gmail.com>
2013-03-04 18:05:43 -0800

    Successfully scrapes logins and logouts from event log.

--- Get-LogonHistory.ps1
+++ Get-LogonHistory.ps1
@@ -7,12 +7,13 @@ $logons = $events | Where-Object { ($_.InstanceId -eq 4647) -or (($_.InstanceId 
 foreach($logon in $logons) {
 	if ($logon.InstanceId -eq 4624) {
 		$logonType = 'on'
-		
-		$logon.message -match "New Logon:\s*Security ID:\s*.*\s*Account Name:\s*(\w+)"
+		$logon.message -match "New Logon:\s*Security ID:\s*.*\s*Account Name:\s*(\w+)" | Out-Null
 		$user = $matches[1]
-		$time = Get-Date $logon.TimeGenerated
-		Write-Host "$user,$logonType,$time"
 	} else {
-		# This is a logoff.
+		$logonType = 'off'
+		$logon.message -match "Subject:\s*Security ID:\s*.*\s*Account Name:\s*(\w+)" | Out-Null
+		$user = $matches[1]
 	}
+	$time = Get-Date $logon.TimeGenerated
+	Write-Host "$user,$logonType,$time"
 }
