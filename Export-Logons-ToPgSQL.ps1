77c9568957d99d555664a14a5a53fe2e6385c884
Craig Meinschein <cmeinsch@gmail.com>
2013-03-07 15:29:09 -0800

    Added script that exports logon information to a PostgreSQL database for easy querying.

---
+++ Export-Logons-ToPgSQL.ps1
@@ -0,0 +1,51 @@
+# Export login data into PostgreSQL DB.
+# Requires that an appropriately named PgSQL ODBC Data Source be created first.
+
+function Export-Logins-ToPGSQL {
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory=$true,
+		           ValueFromPipelineByPropertyName=$True)]
+		[String]$UserName,
+		[Parameter(Mandatory=$true,
+		           ValueFromPipelineByPropertyName=$True)]
+		[String]$ComputerName,
+		[Parameter(Mandatory=$true,
+		           ValueFromPipelineByPropertyName=$True)]
+		[String]$Action,
+		[Parameter(Mandatory=$false,
+		           ValueFromPipelineByPropertyName=$True)]
+		[String]$LogonType,
+		[Parameter(Mandatory=$true,
+		           ValueFromPipelineByPropertyName=$True)]
+		[DateTime]$TimeStamp
+	)
+	BEGIN{
+		$DBConn = New-Object System.Data.Odbc.OdbcConnection('DSN=Pgsql_logondb')
+		$DBCmd = $DBConn.CreateCommand()
+		$DBCmd.CommandText = 'INSERT INTO logins (username,compname,logontype,action,date,time) VALUES (?,?,?,?,?,?);'
+		[void]$DBCmd.Parameters.Add('@username', [System.Data.Odbc.OdbcType]::varchar, 20)
+		[void]$DBCmd.Parameters.Add('@compname', [System.Data.Odbc.OdbcType]::varchar, 20)
+		[void]$DBCmd.Parameters.Add('@logontype', [System.Data.Odbc.OdbcType]::varchar, 10)
+		[void]$DBCmd.Parameters.Add('@action', [System.Data.Odbc.OdbcType]::varchar, 10)
+		[void]$DBCmd.Parameters.Add('@date', [System.Data.Odbc.OdbcType]::date)
+		[void]$DBCmd.Parameters.Add('@time', [System.Data.Odbc.OdbcType]::time)
+		$DBCmd.Connection.Open()
+	}
+	
+	PROCESS{
+		[DateTime]$date = $TimeStamp.date
+		[TimeSpan]$time = Get-Date $TimeStamp -Format 'HH:mm:ss'
+		$DBCmd.Parameters['@username'].Value = $UserName
+		$DBCmd.Parameters['@compname'].Value = $ComputerName
+		$DBCmd.Parameters['@logontype'].Value = $LogonType
+		$DBCmd.Parameters['@action'].Value = $Action
+		$DBCmd.Parameters['@date'].Value = $date
+		$DBCmd.Parameters['@time'].Value = $time
+		[void]$DBCmd.ExecuteNonQuery()
+	}
+	
+	END{
+		$DBCmd.Connection.Close()
+	}
+}
